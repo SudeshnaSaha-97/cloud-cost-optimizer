f2f2ff9c6d5fcffaaf482e8c4b6a8e168505d7ca
Sudeshna Saha <sudeshnasaha97@gmail.com>
2026-03-10 12:55:55 +0530

    added dependency to all kubernetes resouces

--- infra/terraform/gke-workloads.tf
+++ infra/terraform/gke-workloads.tf
@@ -1,21 +1,29 @@
 # Backend Deployment
 resource "kubernetes_manifest" "backend_deployment" {
   manifest = yamldecode(file("${path.module}/../k8s/backend-deployment.yaml"))
+
+  depends_on = [google_container_cluster.primary]
 }
 
 # Backend Service
 resource "kubernetes_manifest" "backend_service" {
   manifest = yamldecode(file("${path.module}/../k8s/backend-service.yaml"))
+
+  depends_on = [google_container_cluster.primary]
 }
 
 # Frontend Deployment
 resource "kubernetes_manifest" "frontend_deployment" {
   manifest = yamldecode(file("${path.module}/../k8s/frontend-deployment.yaml"))
+
+  depends_on = [google_container_cluster.primary]
 }
 
 # Frontend Service
 resource "kubernetes_manifest" "frontend_service" {
   manifest = yamldecode(file("${path.module}/../k8s/frontend-service.yaml"))
+
+  depends_on = [google_container_cluster.primary]
 }
 
 resource "kubernetes_ingress_v1" "cloud_cost_ingress" {
