9f8676a90f92478c87030fe11a1ec47dd165dc49
Sudeshna Saha <sudeshnasaha97@gmail.com>
2026-03-10 12:48:48 +0530

    updated provider to use GKE credentials instead of config_path

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -12,14 +12,23 @@ terraform {
   }
 }
 
+# Google provider
 provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
 }
 
+# Get current GCP client config (access token, project, etc.)
+data "google_client_config" "default" {}
+
+# Kubernetes provider configured with GKE cluster credentials
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
 }
 
-provider "http" {}
+provider "http" {
+
+}
