7b0c587d87a44fa50b635f4479e40fec374de509
Sudeshna Saha <sudeshnasaha97@gmail.com>
2026-03-11 10:10:55 +0530

    Remove load_config_file from the provider block

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -27,9 +27,6 @@ provider "kubernetes" {
   host                   = google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
-
-  # Ensure provider waits until cluster is created
-  load_config_file       = false
 }
 
 provider "http" {
