563b3dcf2dc400f0342f7e3d47cea6c1998cf8f6
Sudeshna Saha <sudeshnasaha97@gmail.com>
2026-03-12 10:40:31 +0530

    Updated providers.tf with compatible versions

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,11 +1,25 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 5.45"   # stable for GKE, Cloud SQL, Artifact Registry
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"   # stable, supports kubernetes_manifest
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.5"
     }
   }
+
   backend "gcs" {
     bucket = "cloud-cost-optimizer-tfstate"
     prefix = "terraform/state"
@@ -25,7 +39,7 @@ data "google_client_config" "default" {}
 # Kubernetes provider configured with GKE cluster credentials
 provider "kubernetes" {
   host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
 }
 
